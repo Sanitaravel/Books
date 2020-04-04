2b67b0123323313c9042313d6e5ee21186401944
Alexvue <58359823+Alexvue@users.noreply.github.com>
2020-04-04 15:00:45 +0300

    Initial commit

---
+++ app/Http/Controllers/BookController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Book;
+
+class BookController extends Controller
+{
+    public function all(){
+        return Book::all();
+    }
+
+    public function add(Request $request){
+        $book = new Book;
+        $book->title = $request->title;
+        $book->author = $request->author;
+        $book->availability = true;
+
+        $book->save();
+        
+        return redirect('/book');
+    }
+
+    public function delete($id){
+        $book = Book::findOrFail($id);
+        $book->delete();
+
+        return redirect('/book');
+    }
+
+    public function changeAvailabilty($id){
+        $book = Book::findOrFail($id);
+        $book->availability = !$book->availability;
+
+        $book->save();
+
+        return redirect('/book');
+    }
+}
---
+++ resources/views/spa.blade.php
@@ -0,0 +1,102 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+    <!-- Подключаем Bootstrap, чтобы не работать над дизайном проекта -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+</head>
+<body>
+
+    <div id="app">
+    @verbatim
+        <div class="container mt-5">
+            <h1>Список книг нашей библиотеки</h1>
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th scope="col">#</th>
+                        <th scope="col">Название</th>
+                        <th scope="col">Автор</th>
+                        <th scope="col">Наличие</th>
+                        <th scope="col">Действия</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr v-for="book in books">
+                        <th scope="row">{{book.id}}</th>
+                        <td>{{book.title}}</td>
+                        <td>{{book.author}}</td>
+                        <td>
+                            <button type="button" class="btn btn-outline-primary" v-on:click="">
+                                Доступна
+                            </button>
+                        </td>
+                        <td>
+                            <button type="button" class="btn btn-outline-danger" v-on:click="">
+                                Удалить
+                            </button>
+                        </td>
+                    </tr>
+
+                    <!-- Строка с полями для добавления новой книги -->
+                    <tr>
+                        <th scope="row">Добавить</th>
+                        <td><input v-model="addingTitle" type="text" class="form-control"></td>
+                        <td><input v-model="addingAuthor" type="text" class="form-control"></td>
+                        <td></td>
+                        <td>
+                            <button @click="addBook()" type="button" class="btn btn-outline-success" v-on:click="">
+                                Добавить
+                            </button>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+        @endverbatim
+    </div>
+    
+
+    <!--Подключаем axios для выполнения запросов к api -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/axios/0.18.0/axios.min.js"></script>
+
+    <!--Подключаем Vue.js -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/vue/2.6.10/vue.min.js"></script>
+
+    <script>
+        let vm = new Vue({
+            el: '#app',
+            data: {
+                addingTitle:'',
+                addingAuthor:'',
+                books:[]
+            },
+            methods: {
+                loadBookList(){
+                    axios.get('book/all').then(response=>{
+                        this.books = response.data;
+                    })
+                },
+                addBook(){
+                    axios.post('/book/add', {
+                        title: this.addingTitle,
+                        author: this.addingAuthor
+                    })
+                    alert(this.addingAuthor + " " + this.addingTitle);
+                },
+                deleteBook(id){
+                    
+                },
+                changeBookAvailability(id){
+                    
+                }
+            },
+            mounted(){
+                // Сразу после загрузки страницы подгружаем список книг и отображаем его
+                this.loadBookList();
+            }
+        });
+    </script>
+</body>
+</html>
